00c1692bd9530772636f2a59803675b6151332fd
tripheo0412 <tripheo0412@gmail.com>
2019-08-03 12:31:05 +0300

    test fail circle ci

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./server');
 
 const { keys } = require('./config');
 
-const PORT = keys.RES_PORT || 5000;
+const PORT = k || 5000;
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Resources server is running at localhost:${PORT}`);
